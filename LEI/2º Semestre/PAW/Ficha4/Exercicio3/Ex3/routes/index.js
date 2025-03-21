8538c411d769a6d5713c6c6f2936510d01a9f240
Miguel <152904615+zeeMiguel03@users.noreply.github.com>
2025-03-21 12:42:43 +0000

    ,

---
+++ LEI/2º Semestre/PAW/Ficha4/Exercicio3/Ex3/routes/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
+const path = require('path');
+
+const filesDir = path.join(__dirname, '..', 'Files');
+
+let counter = fs.readdirSync(filesDir).length;
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/form', (req, res) => {
+  const {name, age, postal, taste, time, ambience} = req.body;
+
+  const dados = JSON.stringify({ name, age, postal, taste, time, ambience });
+
+  counter += 1;
+
+  const filePath = path.join(filesDir, `dados${counter}.txt`);
+
+  fs.appendFile(filePath, dados , (err) => {
+    if (err) {
+      console.error('Error saving data:', err);
+      return res.status(500).send('Error saving data');
+    } 
+
+    console.log('Data saved with sucess!');
+  })
+
+  res.render('index');
+})
+
+module.exports = router;
